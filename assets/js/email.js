de1fcecfe7cb4a94db7c87fa1ab9a6e316624543
ShafinH <shafin1025@gmail.com>
2020-09-26 11:04:06 -0700

    Update email.js

---
+++ assets/js/email.js
@@ -0,0 +1,23 @@
+const { getMaxListeners } = require("process");
+
+var username = "shafin1025@gmail.com";
+var password = "*********"
+var to = firebase.database().ref("emails").val;
+var from = 'shafin1025@gmail.com';
+var subject = "A new event has been created in your area!"
+var body = firebase.database().ref("events").val;
+
+
+function sendEmail(host, username, password, to, from, subject, body) {
+    Email.send({
+        Host: host,
+        Username: username,
+        Password: password,
+        To: to,
+        From: from,
+        Subject: subject,
+        Body: body
+    }).then(
+        message => alert(message)
+    );
+}
