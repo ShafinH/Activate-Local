3afec4079d38fb13a3174cbdc5496e4c4527838e
ShafinH <shafin1025@gmail.com>
2020-09-26 18:12:38 -0700

    Update remove.js

--- Website/assets/js/remove.js
+++ Website/assets/js/remove.js
@@ -2,13 +2,17 @@ var today = new Date();
 var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
 var rootRef = firebase.database().ref("events")
 var ref = firebase.database().ref("events").val('event_date');
+var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
 
 if (date == ref.val()) {
 
-    firebase.collection("events").doc("").delete().then(function () {
-        console.log("Document successfully deleted!");
-    }).catch(function (error) {
-        console.error("Error removing document: ", error);
-    });
+    if (time == '11:59:59') {
+        firebase.collection("events").doc("").delete().then(function () {
+            console.log("Document successfully deleted!");
+        }).catch(function (error) {
+            console.error("Error removing document: ", error);
+        });
+    }
+
 
 }
